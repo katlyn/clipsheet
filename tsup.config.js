a20f8f3ded3c643f54c87bc0d34ba3385a78aeb3
Katlyn Lorimer <katlyn@katlyn.dev>
2024-05-11 01:16:48 -0800

    initial commit

---
+++ tsup.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false,
+  clean: true,
+});
