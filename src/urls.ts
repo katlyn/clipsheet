05a8925ec787855fe2b521c4af8c4e884cab5dde
Katlyn Lorimer <katlyn@katlyn.dev>
2024-06-14 20:10:49 -0800

    feat: add basic key/gid extraction

---
+++ src/urls.ts
@@ -0,0 +1,42 @@
+/**
+ * Utilities relating to parsing sheet keys and GIDs from a given URL.
+ * @module
+ *
+ * The contents of this module are heavily inspired and referenced from the
+ * implementation in Sheetrock by Chris Zarate, licensed under the MIT license.
+ * https://github.com/chriszarate/sheetrock
+ */
+
+/**
+ * Regexes used to extract a sheet key and GID from url.
+ * https://github.com/chriszarate/sheetrock/blob/0eaf2c63fe6a88a772b954c18dcfed636887a572/src/lib/config.js#L21-L33
+ */
+export const urlRegexes = {
+  2014: {
+    keyFormat: new RegExp("spreadsheets/d/([^/#]+)", "i"),
+    gidFormat: new RegExp("gid=([^/&#]+)", "i"),
+  },
+  2010: {
+    keyFormat: new RegExp("key=([^&#]+)", "i"),
+    gidFormat: new RegExp("gid=([^/&#]+)", "i"),
+  },
+};
+
+type SheetDetails = { key: string; gid: string };
+
+/**
+ * Extract a sheet's key and GID from the sheet URL
+ * https://github.com/chriszarate/sheetrock/blob/0eaf2c63fe6a88a772b954c18dcfed636887a572/src/lib/options.js#L45-L61
+ * @param url The URL of the sheet to extract information for
+ */
+export default function extractSheetDetails(url: string): SheetDetails {
+  for (const { keyFormat, gidFormat } of Object.values(urlRegexes)) {
+    if (keyFormat.test(url) && gidFormat.test(url)) {
+      return {
+        key: url.match(keyFormat)![1],
+        gid: url.match(gidFormat)![1],
+      };
+    }
+  }
+  throw new Error("Unable to extract key and GID from url");
+}
