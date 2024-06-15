3478b1076a41bedbc2b80ca2175a872db4feb88f
Katlyn Lorimer <katlyn@katlyn.dev>
2024-06-15 01:28:43 -0800

    chore: update eslint and test config to better serve needs

---
+++ test/_coverage.ts
@@ -0,0 +1,4 @@
+// This file is a stub intended to include all files for test coverage.
+import t from "tap";
+
+t.pass();
--- test/coverage-map.js
+++ test/coverage-map.js
@@ -1,6 +1,11 @@
 /**
  * Return the path to the file(s) that should be covered by the given test file.
- * @param {string} testFile
- * @returns {string}
+ * @param {string} path
+ * @returns {string|string[]|null}
  */
-export default (testFile) => testFile.replace(/^test/, "src");
+export default (path) => {
+  if (path.endsWith("_coverage.ts")) {
+    return [];
+  }
+  return path.replace(/^test/, "src");
+};
